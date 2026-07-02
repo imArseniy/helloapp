605f2dbfe035bb4aa61615102c32c759b644d5f7
black <black@example.com>
2026-07-02 22:50:44 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal static class Program
+{
+    private static void Main()
+    {
+        Hello();
+    }
+
+    private static void Hello()
+    {
+        Console.WriteLine("hello");
+    }
+}
